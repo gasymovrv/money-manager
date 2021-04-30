a290030b0f164a6e6bbdc9d14e1d0747fad18f7f
Ruslan Gasymov <RGasymov@luxoft.com>
2021-04-30 11:42:31 +0400

    Fix jpackage util

--- pack_native_app.bat
+++ pack_native_app.bat
@@ -1,13 +1,17 @@
+@rd /s /q "installer"
+
 jpackage ^
 --name money-manager ^
 --input target ^
 --win-console ^
 --win-dir-chooser ^
 --win-shortcut ^
+--win-per-user-install ^
 --main-jar money-manager-0.0.1.jar ^
 --type exe ^
 --java-options "-Duser.timezone=UTC -Dspring.config.import=optional:file:app/.env[.properties]" ^
 --app-version 0.0.1 ^
---dest installer
+--dest installer ^
+--vendor R.Gasymov
 
 pause
